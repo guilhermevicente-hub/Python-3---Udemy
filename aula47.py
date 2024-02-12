0e4e44785fceb89598aee28ca8c05cfab43807e1
Guilherme <guilherme.vicszz@gmail.com>
2024-02-12 13:42:23 -0300

    Jogo da palavra secreta

--- aula47.py
+++ aula47.py
@@ -1,46 +1,49 @@
 """
-Faça um jogo para o usuario adivinhar qual a palavra secreta.
-- Você vai propor uma palavra secreta qualquer e vai dar da possibulidade para o 
-usuário digitar uma letra.
-- Qual o usuário digitar uma letra, você vai conferir
-se a letra digitada está na palavra secreta.
+Faça um jogo para o usuário adivinhar qual
+a palavra secreta.
+- Você vai propor uma palavra secreta
+qualquer e vai dar a possibilidade para
+o usuário digitar apenas uma letra.
+- Quando o usuário digitar uma letra, você 
+vai conferir se a letra digitada está
+na palavra secreta.
     - Se a letra digitada estiver na
     palavra secreta; exiba a letra;
-    - Se a letra digitada não estiver 
-    na palavr secreta; exiba *.
-Faça a contagem de tentativas do seu usuário
+    - Se a letra digitada não estiver
+    na palavra secreta; exiba *.
+Faça a contagem de tentativas do seu
+usuário.
 """
+import os
 
-
-palavra_secreta = 'macaco'
-acertadas = ''
-tentativa = 0
+palavra_secreta = 'perfume'
+letras_acertadas = ''
+numero_tentativas = 0
 
 while True:
     letra_digitada = input('Digite uma letra: ')
-    tentativa += 1
+    numero_tentativas += 1
 
     if len(letra_digitada) > 1:
         print('Digite apenas uma letra.')
         continue
 
     if letra_digitada in palavra_secreta:
-        acertadas += letra_digitada
-
+        letras_acertadas += letra_digitada
 
     palavra_formada = ''
     for letra_secreta in palavra_secreta:
-        if letra_secreta in acertadas:
+        if letra_secreta in letras_acertadas:
             palavra_formada += letra_secreta
         else:
             palavra_formada += '*'
-        
-        print('Palavra formada:', palavra_formada)
-    
+
+    print('Palavra formada:', palavra_formada)
+
     if palavra_formada == palavra_secreta:
-        print('Você ganhou!! Parabens')
+        os.system('clear')
+        print('VOCÊ GANHOU!! PARABÉNS!')
         print('A palavra era', palavra_secreta)
-        print('Tentativas: ', tentativa)
-            
-            
-            
+        print('Tentativas:', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
